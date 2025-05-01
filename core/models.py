dd2761c9ebab596e977bfc3667325a6eafe26969
nsrodnykh <nsrodnyh@gmail.com>
2025-05-01 19:42:14 +0300

    refactor: подчистил неиспользуемые импорты

--- core/models.py
+++ core/models.py
@@ -4,7 +4,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db.models.signals import post_delete
 import uuid
-from django.utils import timezone
 
 
 
