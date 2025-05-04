0b81f69b2edbbc6d4d7e6649b50dca66515879a8
Aleksandra <sashavar2003@mail.ru>
2025-05-04 16:58:49 +0300

    подключены staticfiles_dirs

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # <путь_до_папки_с_упомянутой_структурой>
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
